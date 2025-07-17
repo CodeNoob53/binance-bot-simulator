4af4bb5b843236659e403eda06fd3fbf6895050a
Fedotov Oleksandr <113617078+CodeNoob53@users.noreply.github.com>
2025-07-17 03:00:42 +0300

    Replace custom test runner with mocha

--- tests/helpers.test.js
+++ tests/helpers.test.js
@@ -1,12 +1,14 @@
-import assert from 'assert';
+import { strict as assert } from 'assert';
 import { parseTimeframe, average } from '../src/utils/helpers.js';
 
-export async function testParseTimeframe() {
-  assert.strictEqual(parseTimeframe('1m'), 60 * 1000);
-  assert.strictEqual(parseTimeframe('2h'), 2 * 60 * 60 * 1000);
-}
+describe('helpers', function () {
+  it('parses timeframe strings', function () {
+    assert.strictEqual(parseTimeframe('1m'), 60 * 1000);
+    assert.strictEqual(parseTimeframe('2h'), 2 * 60 * 60 * 1000);
+  });
 
-export async function testAverage() {
-  assert.strictEqual(average([1, 2, 3]), 2);
-  assert.strictEqual(average([]), 0);
-}
+  it('computes average of numbers', function () {
+    assert.strictEqual(average([1, 2, 3]), 2);
+    assert.strictEqual(average([]), 0);
+  });
+});
